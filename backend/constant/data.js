cfedef599e806ec8203a4188b317188a3f7b84dc
deva22gupta <devanshi0203@gmail.com>
2022-07-25 19:18:36 +0530

    adding data

---
+++ backend/constant/data.js
@@ -0,0 +1,13 @@
+
+export const data = [
+    {
+        title: 'Cristiano Ronaldo to wear jersey number seven at Manchester United again',
+        author: 'Anmol Sharma',
+        description: 'Manchester United have confirmed that Cristiano Ronaldo will wear the number seven jersey after returning to Old Trafford. The five-time Ballon dOr wore the shirt number during his first spell with the club from 2003-2009. It has previously been worn by club legends like George Best, Bryan Robson, Eric Cantona and David Beckham. Ronaldo inherited the number from Edinson Cavani.',
+        url: 'https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2021/09_sep/3_fri/img_1630691914830_548.jpg',
+        timestamp: '2021-09-04T03:55:14.000Z',
+        link: 'https://www.manutd.com/en/amp/news/detail/cristiano-ronaldo-to-wear-the-number-7-shirt-for-man-utd?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts',
+        publisher: 'Man Utd'
+    }
+
+];
